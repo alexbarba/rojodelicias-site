e79fe2ed6952a0bc912f97b8b12f9f56d4afe581
Alexis Barba <alexbarba.2278@gmail.com>
2019-05-20 08:30:36 -0600

    Spinner loader en página menú añadido. Elementos repetidos eliminados del api.json

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -1,6 +1,11 @@
 //<![CDATA[
-        $(window).on('load', function() { // makes sure the whole site is loaded
-            $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
-            $('body').delay(350).css({'overflow':'visible'});
-        })
+$(window).on("load", function() {
+	// makes sure the whole site is loaded
+	$("#preloader")
+		.delay(350)
+		.fadeOut("slow"); // will fade out the white DIV that covers the website.
+	$("body")
+		.delay(350)
+		.css({overflow: "visible"});
+});
 //]]>
