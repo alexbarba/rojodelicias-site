ec85f6c284485a57df444e0d12d86a0345d1048c
Alexis Barba <alexbarba.2278@gmail.com>
2019-05-20 19:07:38 -0600

    Spinner activado en página principal

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -2,10 +2,10 @@
 window.onload = function() {
 	// makes sure the whole site is loaded
 	$("#preloader")
-		.delay(350)
+		.delay(550)
 		.fadeOut("slow"); // will fade out the white DIV that covers the website.
 	$("body")
-		.delay(350)
+		.delay(550)
 		.css({overflow: "visible"});
 };
 
